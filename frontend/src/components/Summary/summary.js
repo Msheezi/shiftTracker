99803bcda15da06d3bf707139518ef69b71be6c3
Msheezi <marshall.randolph@gmail.com>
2020-09-27 12:58:48 -0700

    Title change, tweaks to metrcis

--- frontend/src/components/Summary/summary.js
+++ frontend/src/components/Summary/summary.js
@@ -1,11 +1,10 @@
-import React, {useEffect, useState, } from 'react'
+import React, { useEffect, useState } from "react";
 import { searchResults, keyShifts } from "../../functionhelpers";
-import styled from 'styled-components'
+import styled from "styled-components";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { ShiftsPage } from '../ShiftIndex/shiftsMain';
-import {Metrics} from './summaryMetrics'
-
+import { ShiftsPage } from "../ShiftIndex/shiftsMain";
+import { Metrics } from "./summaryMetrics";
 
 const Container = styled.div`
   /* margin-top: 100px; */
@@ -13,77 +12,72 @@ const Container = styled.div`
   grid-template-areas:
     ". header  header ."
     ". shifts  rangeSummary .";
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 0.5fr;
 `;
 
+export const Summary = () => {
+  //display blanks update values after entering in the starting and ending dates
 
-export const Summary = ()=>{
-    //display blanks update values after entering in the starting and ending dates
+  const [summaryState, setSummaryState] = useState();
+  const [startDate, setStartDate] = useState({ start: "" });
+  const [endDate, setEndDate] = useState({ end: "" });
+  const [displayMetrics, setDisplayMetrics] = useState(true);
 
-    const [summaryState, setSummaryState] = useState()
-    const [startDate, setStartDate] = useState({start: ""})
-    const [endDate, setEndDate] = useState({end: ""})
-    const [displayMetrics, setDisplayMetrics] = useState(true)
-    
-    const search = async () => {
-      if(startDate && endDate) {
-        let data = await searchResults(startDate,endDate)
-        setSummaryState(data.data)
-      }
+  const search = async () => {
+    if (startDate && endDate) {
+      let data = await searchResults(startDate, endDate);
+      setSummaryState(data.data);
     }
+  };
 
-    
-   
-    const handleClose = () => {
-      setDisplayMetrics(true);
-    }
+  const handleClose = () => {
+    setDisplayMetrics(true);
+  };
 
-    const hideMetrics = () => {
-      setDisplayMetrics(false)
-    }
+  const hideMetrics = () => {
+    setDisplayMetrics(false);
+  };
 
+  let keyedShifts = summaryState ? keyShifts(summaryState.shifts) : "";
 
-    let keyedShifts = summaryState ? keyShifts(summaryState.shifts) : ""
-    
-    let metrics = summaryState && displayMetrics ? <Metrics shiftTotals={summaryState.shiftTotals} /> : ""
-    let shifts = summaryState ? (
-      <ShiftsPage
-        shifts={keyedShifts}
-        location={true}
-        handleClose={handleClose}
-        hideMetrics={hideMetrics}
-      />
+  let metrics =
+    summaryState && displayMetrics ? (
+      <Metrics shiftTotals={summaryState.shiftTotals} />
     ) : (
       ""
     );
+  let shifts = summaryState ? (
+    <ShiftsPage
+      shifts={keyedShifts}
+      location={true}
+      handleClose={handleClose}
+      hideMetrics={hideMetrics}
+    />
+  ) : (
+    ""
+  );
 
+  return (
+    <Container>
+      <div style={{ gridArea: "header" }}>
+        <DatePicker
+          id="start"
+          onChange={(date) => setStartDate(date)}
+          selected={Date.parse(startDate)}
+        />
 
-    return (
-      <Container>
-        <div style={{ gridArea: "header" }}>
-          <DatePicker
-            id="start"
-            onChange={(date) => setStartDate(date)}
-            selected={Date.parse(startDate)}
-          />
+        <DatePicker
+          id="end"
+          onChange={(date) => setEndDate(date)}
+          selected={Date.parse(endDate)}
+        />
+        <button onClick={search}>Get Shifts</button>
+      </div>
 
-          <DatePicker
-            id="end"
-            onChange={(date) => setEndDate(date)}
-            selected={Date.parse(endDate)}
-          />
-          <button onClick={search}>Get Shifts</button>
-        </div>
-      
-        <div
-          
-          style={{ gridArea: "shifts" }}
-        >
-          {shifts}
-        </div>
-        <div style={{ gridArea: "rangeSummary", marginTop: "55px" }}>
-          <div>{metrics}</div>
-        </div>
-      </Container>
-    );
-}
+      <div style={{ gridArea: "shifts" }}>{shifts}</div>
+      <div style={{ gridArea: "rangeSummary", marginTop: "55px" }}>
+        <div>{metrics}</div>
+      </div>
+    </Container>
+  );
+};
--- frontend/src/components/Summary/summaryMetrics.js
+++ frontend/src/components/Summary/summaryMetrics.js
@@ -1,6 +1,5 @@
-
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
   width: 300px;
@@ -11,6 +10,7 @@ const Container = styled.div`
   padding: 10px 20px;
   text-align: center;
   box-shadow: -2px -1px 2px lightgray, 2px 2px 2px lightgray;
+  margin-right: 20px;
 `;
 
 const MetricItem = styled.div`
@@ -34,29 +34,26 @@ const MetricItem = styled.div`
 `;
 
 const StyledHeader = styled.h3`
-    color: dimgray;
-    font-family: Roboto;
-    font-size: 14pt;
-    text-align: center;
-    font-weight: bold;
+  color: dimgray;
+  font-family: Roboto;
+  font-size: 14pt;
+  text-align: center;
+  font-weight: bold;
+`;
 
-`
+export const Metrics = ({ shiftTotals }) => {
+  const { duration, tips, ttlComp, ttlMiles } = shiftTotals;
 
-export const Metrics = ({shiftTotals}) => {
-    const {duration, tips, ttlComp, ttlMiles} = shiftTotals
-
-    return (
-      <Container>
-        <StyledHeader>Shift Metrics</StyledHeader>
-        <MetricItem>{`Miles Driven: ${ttlMiles}`}</MetricItem>
-        <MetricItem>{`Hours Worked: ${duration.toFixed(2)}`}</MetricItem>
-        <MetricItem>{`Tips Collected: ${tips.toFixed(2)}`}</MetricItem>
-        <MetricItem>{`Est. Earnings: $ ${ttlComp}`}</MetricItem>
-        <p>
-          *Totals only include <strong>Closed</strong> shifts
-        </p>
-      </Container>
-    );
-
-
-}
+  return (
+    <Container>
+      <StyledHeader>Shift Metrics</StyledHeader>
+      <MetricItem>{`Miles Driven: ${ttlMiles}`}</MetricItem>
+      <MetricItem>{`Hours Worked: ${duration.toFixed(2)}`}</MetricItem>
+      <MetricItem>{`Tips Collected: ${tips.toFixed(2)}`}</MetricItem>
+      <MetricItem>{`Est. Earnings: $ ${ttlComp}`}</MetricItem>
+      <p>
+        *Totals only include <strong>Closed</strong> shifts
+      </p>
+    </Container>
+  );
+};
